1f637808a078b91de7692ca9dac1bbcc5e0709e3
Литвяков Дмитрий Владимирович <player859@yandex.by>
2026-09-06 14:58:20 +0300

    feat(catalog-domain): add BookCopy entity
    - Represented physical book instances linked to BookId
    
    - Included unique InventoryNumber for library tracking

---
+++ backend/src/Modules/Catalog/Catalog.Domain/Models/BookCopy.cs
@@ -0,0 +1,20 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace Catalog.Domain.Models
+{
+    public sealed class BookCopy : Entity<BookCopyId>
+    {
+        public BookId BookId { get; }
+        public InventoryNumber InventoryNumber { get; }
+
+        private BookCopy(BookCopyId id, BookId bookId, InventoryNumber inventoryNumber) : base(id)
+        {
+            BookId = bookId;
+            InventoryNumber = inventoryNumber;
+        }
+
+        public static Result<BookCopy> Create(BookCopyId bookCopyId, BookId bookId, InventoryNumber inventoryNumber)
+            => new BookCopy(bookCopyId, bookId, inventoryNumber);
+        
+    }
+}
